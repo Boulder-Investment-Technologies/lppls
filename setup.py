3cdc7599c04d5997958c9d6de3eb9ce2ebcf66f1
Josh Nielsen <josh.nielsen@broadcom.com>
2020-04-17 16:14:45 -0600

    updating readme and setup

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup
+import setuptools
 
-setup(name='lppls',
-      version='0.1.4',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='lppls',
+      version='0.1.5',
       description='A Python module for fitting the LPPLS model to data.',
       packages=['lppls'],
       author='Josh Nielsen',
       author_email='josh@boulderinvestment.tech',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/Boulder-Investment-Technologies/lppls',
+      python_requires='>=3.6',
       zip_safe=False)
