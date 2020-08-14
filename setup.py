2ab5eb5871ffd4dba3b646746944241b57d7d8af
Josh Nielsen <jnielsen01@gmail.com>
2020-08-14 16:45:00 -0600

    bump version 0.2.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='lppls',
-      version='0.2.7',
+      version='0.2.8',
       description='A Python module for fitting the LPPLS model to data.',
       packages=['lppls'],
       author='Josh Nielsen',
