c110920143de8b49dfc72d31206bd0ccaa58de04
Daniel Scott <info@synapseproduction.com>
2020-08-30 23:44:19 -0600

    Updated tests when not using ln

--- lppls/tests/test_lppls.py
+++ lppls/tests/test_lppls.py
@@ -29,14 +29,15 @@ def test_lppls(data):
     assert 7.6306097488441615 == lppls_model.lppls(t, tc, m, w, a, b, c1, c2)
 
     # Not using ln
+    lppls_model = lppls.LPPLS(use_ln=False, observations=data)
     # Check integrity at period 0
     t, tc, m, w, a, b, c1, c2 = 0.0, 1300.2412888852296, 0.6087189222292106, 6.344318139503496, 3034.166016949172,\
                                 - 16.041970137173486, 0.21878280136703082, - 0.14789336333436504
-    assert 1762.3196588471408 == lppls_model.lppls(t, tc, m, w, a, b, c1, c2)
+    assert 1793.5438277142375 == lppls_model.lppls(t, tc, m, w, a, b, c1, c2)
 
     # Check integrity at period 500
     t, tc, m, w, a, b, c1, c2 = 500.0, 1428.0641475858731, 0.3473013071950998, 6.052643019980449, 3910.1099206097356,\
                                 -169.93053270790418, 0.05189394517600043, -0.045820295077658835
-    assert 2086.3299554496016 == lppls_model.lppls(t, tc, m, w, a, b, c1, c2)
+    assert 2087.089947771132 == lppls_model.lppls(t, tc, m, w, a, b, c1, c2)
 
     return
