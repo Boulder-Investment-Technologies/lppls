e49359166582fa877369dc31e755f1fae5fa4a58
Joshwani <josh@boulderinvestment.tech>
2021-01-15 22:26:56 -0700

    update data_loader test to do an approx check to 1e6 prec

--- lppls/data_loader.py
+++ lppls/data_loader.py
@@ -6,4 +6,4 @@ def sp500():
     # This is a stream-like object. If you want the actual info, call
     # stream.read()
     stream = pkg_resources.resource_stream(__name__, 'data/sp500.csv')
-    return pd.read_csv(stream, encoding='latin-1')
+    return pd.read_csv(stream, encoding='utf-8')
--- lppls/tests/test_data_loader.py
+++ lppls/tests/test_data_loader.py
@@ -1,9 +1,11 @@
 import data_loader
-import pandas as pd
+import pytest
 
 
 def test_sp500():
     # Test that stream loads
     data = data_loader.sp500()
-    assert data.iloc[0].values.tolist() == ['2013-10-30', 1772.27002, 1775.219971, 1757.2399899999998, 1763.310059,
-                                            1763.310059, 3523040000]
+    actual = data.iloc[0].values.tolist()
+    expected = ['2013-10-30', 1772.27002, 1775.219971, 1757.2399899999998, 1763.310059,
+                1763.310059, 3523040000]
+    assert all([a == pytest.approx(b, 1e6) for a, b in zip(actual, expected)])
