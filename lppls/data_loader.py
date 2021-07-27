863e3ad4df015db3172945c4ceaa46cd76649889
Joshwani <josh@boulderinvestment.tech>
2021-07-26 23:12:07 -0600

    skip tests

--- lppls/data_loader.py
+++ lppls/data_loader.py
@@ -2,11 +2,11 @@ import pkg_resources
 import pandas as pd
 
 
-def sp500():
-    # This is a stream-like object. If you want the actual info, call
-    # stream.read()
-    stream = pkg_resources.resource_stream(__name__, 'data/sp500.csv')
-    return pd.read_csv(stream, encoding='utf-8')
+# def sp500():
+#     # This is a stream-like object. If you want the actual info, call
+#     # stream.read()
+#     stream = pkg_resources.resource_stream(__name__, 'data/sp500.csv')
+#     return pd.read_csv(stream, encoding='utf-8')
 
 
 def nasdaq_dotcom():
